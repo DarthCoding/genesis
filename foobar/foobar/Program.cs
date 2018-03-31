5561f498ead9e66c9a7e2e28e26d62c442f40e85
beaumont117@gmail.com <beaumont117@gmail.com>
2018-03-31 10:12:37 -0500

    how to collect multiple arguements

--- foobar/foobar/Program.cs
+++ foobar/foobar/Program.cs
@@ -1,12 +1,26 @@
 ﻿using System;
 
+
+//Lets start by learning how to pass in arguements and then split them into variables :)
+
 namespace foobar
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var env = args.Length > 0 ? args[0] : String.Empty;
+
+            /*Console.WriteLine(env);
+            Console.WriteLine(args.Length);
+            Console.WriteLine(args[0]);
+            Console.WriteLine(args[1]); */
+
+            
+
+
+
+
         }
     }
 }
